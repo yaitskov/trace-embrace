85f361b0c9af81553d17a23a881cca7243d16a94
Daniil Iaitskov <dyaitskov@gmail.com>
2025-02-18 13:48:33 -0900

    init

---
+++ default.nix
@@ -0,0 +1,61 @@
+{ system ? builtins.currentSystem or "x86_64-linux"
+, ghc ? "ghc9101"
+}:
+
+let
+  nix = import ./nix;
+  pkgs = nix.pkgSetForSystem system {
+    config = {
+      allowBroken = true;
+      allowUnfree = true;
+    };
+  };
+
+  inherit (pkgs) lib;
+
+  hsPkgSetOverlay = pkgs.callPackage ./nix/haskell/overlay.nix {
+    inherit (nix) sources;
+  };
+
+  sources = [
+    "^src.*$"
+    "^tests.*$"
+    "^LICENSE.*$"
+    "^.*\\.cabal$"
+  ];
+
+  base = hsPkgs.callCabal2nix "trace-if" (lib.sourceByRegex ./. sources) { };
+  trace-if-overlay = _hf: _hp: { trace-ifc = base; };
+  baseHaskellPkgs = pkgs.haskell.packages.${ghc};
+  hsOverlays = [ hsPkgSetOverlay trace-if-overlay ];
+  hsPkgs = baseHaskellPkgs.override (old: {
+    overrides =
+      builtins.foldl' pkgs.lib.composeExtensions (old.overrides or (_: _: { }))
+      hsOverlays;
+  });
+
+  hls = pkgs.haskell.lib.overrideCabal hsPkgs.haskell-language-server
+    (_: { enableSharedExecutables = true; });
+
+  shell = hsPkgs.shellFor {
+    packages = p: [ p.trace-if ];
+    nativeBuildInputs = (with pkgs; [
+      cabal-install
+      ghcid
+      hlint
+      niv
+    ]) ++ [ hls ];
+    shellHook = ''
+      export PS1='$ '
+      echo $(dirname $(dirname $(which ghc)))/share/doc > .haddock-ref
+    '';
+  };
+
+  trace-if = hsPkgs.trace-ifc;
+in {
+  inherit hsPkgs;
+  inherit ghc;
+  inherit pkgs;
+  inherit shell;
+  inherit trace-if;
+}
