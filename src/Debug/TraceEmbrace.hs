f5642b0d32cadefb119c05e8cb545cd3d3d1cbc7
Daniil Iaitskov <dyaitskov@gmail.com>
2025-03-05 12:18:57 -0900

    master export ShowTrace by default

--- src/Debug/TraceEmbrace.hs
+++ src/Debug/TraceEmbrace.hs
@@ -1,5 +1,6 @@
 -- | The root module to be imported by applications and libraries.
-module Debug.TraceEmbrace (module TT) where
+module Debug.TraceEmbrace (ShowTrace (..), module TT) where
 
 import Debug.TraceEmbrace.Config as TT
+import Debug.TraceEmbrace.ByteString (ShowTrace (..))
 import Debug.TraceEmbrace.TH as TT
